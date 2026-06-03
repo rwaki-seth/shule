37347a7b819f032cab49c678cf778f237de08ac6
rwaki-seth <89144082+rwaki-seth@users.noreply.github.com>
2026-06-03 14:11:57 +0300

    Add Vercel results API

---
+++ api/results.js
@@ -0,0 +1,10 @@
+const { calculateResults, readDb, sendError, sendJson } = require("./_lib/shule");
+
+module.exports = function handler(req, res) {
+  try {
+    if (req.method !== "GET") return sendError(res, 405, "Method not allowed");
+    return sendJson(res, 200, calculateResults(readDb()));
+  } catch (error) {
+    return sendError(res, 400, error.message || "Request failed");
+  }
+};
